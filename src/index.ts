0f1ff654ea3be58825d794fe8fe1808727e47907
Yuri <yuri.pires.silveira@gmail.com>
2025-10-13 18:08:09 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
