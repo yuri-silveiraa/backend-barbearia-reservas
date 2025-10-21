b793fe046abb9614bb31848dbff212122c1a8fb9
Yuri <yuri.pires.silveira@gmail.com>
2025-10-21 19:30:48 -0300

    criando use-cases createUser e ListAppointment

--- src/core/entities/Balance.ts
+++ src/core/entities/Balance.ts
@@ -2,6 +2,7 @@ export class Balance {
   constructor(
     public readonly id: string,
     public readonly barberId: string,
-    public balance: number = 0
+    public balance: number = 0,
+    public atualizedAt: Date = new Date()
   ) {}
 }
