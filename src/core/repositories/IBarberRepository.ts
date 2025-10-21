ac60d5d852e422ce1ea39a85568baaba54c61255
Yuri <yuri.pires.silveira@gmail.com>
2025-10-21 20:58:58 -0300

    ajustes no use-case CreateUser

--- src/core/repositories/IBarberRepository.ts
+++ src/core/repositories/IBarberRepository.ts
@@ -1,6 +1,6 @@
 import { Barber } from '../entities/Barber';
 
 export interface IBarbersRepository {
-  create(data: Omit<Barber, 'id' | 'createdAt'>): Promise<Barber>;
+  create(data: Omit<Barber, 'id' | 'createdAt' | 'isAdmin'>): Promise<Barber>;
   findByUserId(userId: string): Promise<Barber | null>;
 }
