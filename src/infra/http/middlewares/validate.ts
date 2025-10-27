a7a353fb9dc21624a2ffab15654f926bccbf7ddc
Yuri <yuri.pires.silveira@gmail.com>
2025-10-27 14:48:34 -0300

    criaando middlewares validate e errohandler

---
+++ src/infra/http/middlewares/validate.ts
@@ -0,0 +1,25 @@
+import { ZodError, ZodObject } from "zod";
+import { Request, Response, NextFunction } from "express";
+
+export const validate =
+  (schema: ZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res.status(400).json({
+          message: "Validação falhou",
+          errors: error.issues.map((issue) => ({
+            path: issue.path.join("."),
+            message: issue.message,
+          })),
+        });
+      }
+
+      return res.status(500).json({
+        message: "Internal server error during validation",
+      });
+    }
+  };
