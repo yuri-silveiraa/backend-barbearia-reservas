43a39ecf8eebb4586b88f8db6d41ae4334d08427
Yuri <yuri.pires.silveira@gmail.com>
2025-10-27 14:47:39 -0300

    criando createUserSchema para validação de dados

---
+++ src/infra/http/schemas/CreateUser.schema.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const CreateUserSchema = z.object({
+  name: z.string().trim().min(3, "Nome precisa ter no mínimo 3 caracteres"),
+  email: z.email("Email inválido").trim(),
+  password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+  type: z.enum(["BARBER", "CLIENT"], {
+    message: "Tipo de usuário inválido.",
+  }),
+  telephone: z.string().trim().min(9, "Telefone invalido")
+});
