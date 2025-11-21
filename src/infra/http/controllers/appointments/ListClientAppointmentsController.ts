c99be4e108ffd2a812d7e16676d81586ee95d4b4
Yuri <yuri.pires.silveira@gmail.com>
2025-11-21 17:26:33 -0300

    ajustes

--- src/infra/http/controllers/appointments/ListClientAppointmentsController.ts
+++ src/infra/http/controllers/appointments/ListClientAppointmentsController.ts
@@ -1,6 +1,6 @@
 import { ListClientAppointments } from "../../../../core/use-cases/ListClientAppointments";
-import { AuthenticatedRequest } from "../../middlewares/ensureAuthenticated";
 import { Response } from "express";
+import { AuthenticatedRequest } from "../../helpers/requestInterface";
 
 export class ListClientAppointmentsController {
   constructor(private listClientAppointments: ListClientAppointments) {}
