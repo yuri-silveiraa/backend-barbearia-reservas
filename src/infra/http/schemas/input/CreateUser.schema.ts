5725adf207d38d910edd84ac2722d7bcb987a865
Yuri <yuri.pires.silveira@gmail.com>
2026-02-22 16:34:45 -0300

    fix: restringir rota /user/create apenas para clientes

--- src/infra/http/schemas/input/CreateUser.schema.ts
+++ src/infra/http/schemas/input/CreateUser.schema.ts
@@ -4,8 +4,5 @@ export const CreateUserSchema = z.object({
   name: z.string().trim().min(3, "Nome precisa ter no mínimo 3 caracteres"),
   email: z.email("Email inválido").trim(),
   password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
-  type: z.enum(["BARBER", "CLIENT"], {
-    message: "Tipo de usuário inválido.",
-  }),
   telephone: z.string().trim().min(11, "Telefone invalido").optional(),
 });
